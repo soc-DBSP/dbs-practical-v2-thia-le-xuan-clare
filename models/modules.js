c8fea71c27ca179442a0a4d3c40aed5aaec58166
sunshinehippiekitty <sunshinehippiekitty@gmail.com>
2026-05-11 12:08:10 +0800

    Completed Practical

--- models/modules.js
+++ models/modules.js
@@ -2,13 +2,13 @@ const { query } = require('../database');
 const { EMPTY_RESULT_ERROR, SQL_ERROR_CODE, UNIQUE_VIOLATION_ERROR } = require('../errors');
 
 module.exports.create = function create(code, name, credit) {
-    const sql = `INSERT INTO module (mod_code, mod_name, credit_unit) VALUES ($1, $2, $3)`;
-    return query(sql, [code, name, credit]).catch(function (error) {
-        if (error.code === SQL_ERROR_CODE.UNIQUE_VIOLATION) {
-            throw new UNIQUE_VIOLATION_ERROR(`Module ${code} already exists`);
-        }
-        throw error;
-    });
+return query('CALL create_module($1, $2, $3)', [code, name, credit])
+.then(function (result) {
+console.log('Module created successfully');
+})
+.catch(function (error) {
+throw error;
+});
 };
 
 module.exports.retrieveByCode = function retrieveByCode(code) {
@@ -26,43 +26,36 @@ module.exports.retrieveByCode = function retrieveByCode(code) {
     });
 };
 
-module.exports.deleteByCode = function deleteByCode(code) {
-    // Note:
-    // If using raw sql: Can use result.rowCount to check the number of rows affected
-    // But if using function/stored procedure, result.rowCount will always return null
-    const sql = `DELETE FROM module WHERE mod_code = $1`;
-    return query(sql, [code]).then(function (result) {
-        const rows = result.rowCount;
-
-        if (rows === 0) {
-            // Note: result.rowCount returns the number of rows processed instead of returned
-            // Read more: https://node-postgres.com/apis/result#resultrowcount-int--null
-            throw new EMPTY_RESULT_ERROR(`Module ${code} not found!`);
-        }
-    })
+module.exports.updateByCode = function updateByCode(code, credit) {
+    // Now using stored procedure instead of raw SQL
+    // Error handling is done in the stored procedure itself
+    return query('CALL update_module($1, $2)', [code, credit])
+        .then(function (result) {
+            console.log('Module updated successfully');
+        })
+        .catch(function (error) {
+            throw error;
+        });
 };
 
-module.exports.updateByCode = function updateByCode(code, credit) {
-    // Note:
-    // If using raw sql: Can use result.rowCount to check the number of rows affected
-    // But if using function/stored procedure, result.rowCount will always return null
-    const sql = `UPDATE module SET credit_unit = $1 WHERE mod_code = $2`;
-    return query(sql, [credit, code]).then(function (result) {
-        const rows = result.rowCount;
-
-        if (rows === 0) {
-            // Note: result.rowCount returns the number of rows processed instead of returned
-            // Read more: https://node-postgres.com/apis/result#resultrowcount-int--null
-            throw new EMPTY_RESULT_ERROR(`Module ${code} not found!`);
-        }
-    })
+module.exports.deleteByCode = function deleteByCode(code) {
+    // Now using stored procedure instead of raw SQL
+    // Error handling is done in the stored procedure itself
+    return query('CALL delete_module($1)', [code])
+        .then(function (result) {
+            console.log('Module deleted successfully');
+        })
+        .catch(function (error) {
+            throw error;
+        });
 };
 
 module.exports.retrieveAll = function retrieveAll() {
-    const sql = `SELECT * FROM module`;
-    return query(sql).then(function (result) {
-        return result.rows;
-    });
+// retrieve students via stored procedure
+const sql = `SELECT adm_no, stud_name, gender, crse_code, gpa, gpa_last_updated FROM student`;
+return query(sql).then(function (result) {
+return result.rows;
+});
 };
 
 module.exports.retrieveBulk = function retrieveBulk(codes) {
--- models/reports.js
+++ models/reports.js
@@ -1,15 +1,15 @@
 const { query } = require('../database');
 
 module.exports.generateModulesPerformance = function generateModulesPerformance() {
-    const sql = 'SELECT * FROM get_modules_performance()';
-    return query(sql)
-        .then(function (result) {
-            const rows = result.rows;
-            return rows;
-        })
-        .catch(function (error) {
-            throw error;
-        });
+const sql = 'SELECT * FROM get_modules_performance() AS result';
+return query(sql)
+.then(function (result) {
+const rows = result.rows;
+return rows;
+})
+.catch(function (error) {
+throw error;
+});
 };
 
 module.exports.calculateStudentsGPA = function calculateStudentsGPA() {
